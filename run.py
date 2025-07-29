448d4a5f08ecb7b76f442a01c9aa39b34ea7a68b
8ARTEZZIO <b4rte4@gmail.com>
2025-07-29 21:06:17 +0100

    checkpoint html base index

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='app/templates')
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(port=5050, debug=True)
