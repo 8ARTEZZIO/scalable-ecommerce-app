2ee4144f0ef543d189a2e6a2954c97b2a52edd88
8ARTEZZIO <b4rte4@gmail.com>
2025-09-03 00:51:17 +0100

    1. develop login / register api 2. add features.html and api 3. temporarily use only User db table

--- app/api.py
+++ app/api.py
@@ -11,16 +11,16 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
 
 from .extensions import db, login_manager
-from .models import Users, Products  # add others later (Cart, Orders, etc.)
+from .models import User  # add others later (Cart, Orders, etc.)
 from .forms import Login, Register
 
-import datetime
+from datetime import date
 
 bp = Blueprint('api', __name__)
 
 @bp.route("/")
 def index():
-    year = datetime.date.today().year
+    year = date.today().year
     return render_template("index.html", year=year)
 
 @bp.route("/products")
@@ -35,6 +35,20 @@ def error():
 def login():
 
     form=Login()
+    if form.validate_on_submit():
+        password = form.password.data
+        result = db.session.execute(db.select(User).where(User.username == form.username.data))
+        user = result.scalar()
+        if not user:
+            flash("That username does not exist, please try again.")
+            return redirect(url_for('api.login'))
+        elif not check_password_hash(user.password_hash, password):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('api.login'))
+        else:
+            login_user(user)
+            flash('Logged in successfully.')
+            return redirect(url_for('api.index'))
     return render_template("login.html", form=form)
 
 @bp.route("/register", methods=["GET", "POST"])
@@ -43,11 +57,17 @@ def register():
     form = Register()
     if form.validate_on_submit():
 
-        new_user = Users
-        username = form.username.data
-        email = form.email.data
         password = form.password.data
         r_pass = form.r_password.data
+
+        new_user = User(username=form.username.data,
+                        email=form.email.data,
+                        password_hash=generate_password_hash(password),
+                        created_at=date.today().strftime("%B %d, %Y")
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
 
         if password == r_pass:
             return redirect(url_for('api.index'))
@@ -61,3 +81,7 @@ def register():
 def logout():
     logout_user()
     return redirect(url_for('api.login'))
+
+@bp.route("/features")
+def features():
+    return render_template("features.html")
--- app/models.py
+++ app/models.py
@@ -8,128 +8,130 @@ from .extensions import db, Base
 from sqlalchemy import String, DateTime, ForeignKey, Table, Column
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from flask_login import UserMixin
+from datetime import datetime
 
 
-user_orders = db.Table(
-    "user_orders",
-    db.Column("cart_id", db.ForeignKey("cart_table.id"), primary_key=True),
-    db.Column("product_id", db.ForeignKey("products_table.id"), primary_key=True),
-)
+# user_orders = db.Table(
+#     "user_orders",
+#     db.Column("cart_id", db.ForeignKey("cart_table.id"), primary_key=True),
+#     db.Column("product_id", db.ForeignKey("products_table.id"), primary_key=True),
+# )
+#
+# order_product = db.Table(
+#     "order_product",
+#     db.Column("order_id", db.ForeignKey("order_table.id"), primary_key=True),
+#     db.Column("product_id", db.ForeignKey("products_table.id"), primary_key=True),
+# )
 
-order_product = db.Table(
-    "order_product",
-    db.Column("order_id", db.ForeignKey("order_table.id"), primary_key=True),
-    db.Column("product_id", db.ForeignKey("products_table.id"), primary_key=True),
-)
 
 
-class Users(UserMixin, db.Model):
-    __tablename__ = "users_table"
+class User(UserMixin, db.Model):
+    # __tablename__ = "users_table"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(unique=True)
-    email: Mapped[str] = mapped_column(unique=True)
-    password_hash: Mapped[str] = mapped_column(String(225))
+    username: Mapped[str] = mapped_column(String(80), unique=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True)
+    password_hash: Mapped[str] = mapped_column(String(255))
     role: Mapped[str] = mapped_column(default="user")
-    created_at: Mapped[str] = mapped_column(DateTime)
-    carts: Mapped["Cart"] = relationship(back_populates="users")
-    orders: Mapped[list["Order"]] = relationship(back_populates="users")
+    created_at: Mapped[str] = mapped_column(default=datetime.now)
+    # cart: Mapped["Cart"] = relationship(back_populates="users")
+    # orders: Mapped[list["Order"]] = relationship(back_populates="users")
 
 
-class Products(db.Model):
-    __tablename__ = "products_table"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(225))
-    slug: Mapped[str] = mapped_column(unique=True)
-    price: Mapped[int]
-    currency: Mapped[str]
-    description: Mapped[str] = mapped_column(String(500))
-    main_image_url: Mapped[str]
-    stock: Mapped[int]
-    is_active: Mapped[bool]
-    created_at: Mapped[str] = mapped_column(DateTime)
-    updated_at: Mapped[str]
-    order: Mapped[list["Order"]] = relationship(
-        secondary=order_product,
-        back_populates="products"
-    )
-    # (Optional, later) product_images: (id, product_id, url, alt_text, position)
-    # (Optional, later) categories + product_categories (many-to-many)
-
-
-class Cart(db.Model):
-    __tablename__ = "cart_table"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    created_at: Mapped[str] = mapped_column(DateTime)
-    updated_at: Mapped[str]
-    user_id: Mapped[int] = mapped_column(ForeignKey("users_table.id"))
-    users: Mapped["Users"] = relationship(back_populates="cart")
-    children: Mapped[list[Products]] = relationship(secondary=user_orders)
-
-
-class Order(db.Model):  # finalized purchase
-    __tablename__ = "order_table"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("users_table.id"), nullable=True)
-    users: Mapped["Users"] = relationship(back_populates="order")
-    products: Mapped[list[Products]] = relationship(
-        secondary=order_product,
-        back_populates="order"
-    )
-    status: Mapped[str]
-    subtotal: Mapped[int]
-    tax_total: Mapped[int]
-    shipping_total: Mapped[int]
-    discount_total: Mapped[int]
-    grand_total: Mapped[int]
-    currency: Mapped[str]
-    placed_at: Mapped[datetime | None] = mapped_column(db.DateTime)
-    billing_address_id: Mapped[str]
-    shipping_address_id: Mapped[str]
-
-    # order_items (association with price snapshot):
-    # id, order_id, product_id, product_name, sku (optional),
-    # unit_price, quantity, line_total
-    #
-    # (Store product_name/unit_price here to freeze what was bought at that time.)
-
-
-class Addresses(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(nullable=True)
-    full_name: Mapped[str]
-    line1: Mapped[str]
-    line2: Mapped[str]
-    city: Mapped[str]
-    region: Mapped[str]
-    postal_code: Mapped[str]
-    country: Mapped[str]
-    phone: Mapped[int] = mapped_column(unique=True)
-
-
-class Payments(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    order_id: Mapped[int] = mapped_column(nullable=True)
-    provider: Mapped[str]
-    provider_ref: Mapped[str]
-    amount: Mapped[int]
-    currency: Mapped[str]
-    status: Mapped[str]
-    captured_at: Mapped[str]
-
-
-class Shipments(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    order_id: Mapped[int] = mapped_column(nullable=True)
-    carrier: Mapped[str]
-    service: Mapped[str]
-    tracking_number: Mapped[str]
-    status: Mapped[str]
-    shipped_at: Mapped[str]
-    delivered_at: Mapped[str]
+# class Products(Base):
+#     __tablename__ = "products_table"
+#
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     name: Mapped[str] = mapped_column(String(225))
+#     slug: Mapped[str] = mapped_column(unique=True)
+#     price: Mapped[int]
+#     currency: Mapped[str]
+#     description: Mapped[str] = mapped_column(String(500))
+#     main_image_url: Mapped[str]
+#     stock: Mapped[int]
+#     is_active: Mapped[bool]
+#     created_at: Mapped[str] = mapped_column(DateTime)
+#     updated_at: Mapped[str]
+#     order: Mapped[list["Order"]] = relationship(
+#         secondary=order_product,
+#         back_populates="products"
+#     )
+#     # (Optional, later) product_images: (id, product_id, url, alt_text, position)
+#     # (Optional, later) categories + product_categories (many-to-many)
+#
+#
+# class Cart(Base):
+#     __tablename__ = "cart_table"
+#
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     created_at: Mapped[str] = mapped_column(DateTime)
+#     updated_at: Mapped[str]
+#     user_id: Mapped[int] = mapped_column(ForeignKey("users_table.id"))
+#     users: Mapped["Users"] = relationship(back_populates="cart")
+#     children: Mapped[list[Products]] = relationship(secondary=user_orders)
+#
+#
+# class Order(Base):  # finalized purchase
+#     __tablename__ = "order_table"
+#
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     user_id: Mapped[int] = mapped_column(ForeignKey("users_table.id"), nullable=True)
+#     users: Mapped["Users"] = relationship(back_populates="order")
+#     products: Mapped[list[Products]] = relationship(
+#         secondary=order_product,
+#         back_populates="order"
+#     )
+#     status: Mapped[str]
+#     subtotal: Mapped[int]
+#     tax_total: Mapped[int]
+#     shipping_total: Mapped[int]
+#     discount_total: Mapped[int]
+#     grand_total: Mapped[int]
+#     currency: Mapped[str]
+#     placed_at: Mapped[datetime | None] = mapped_column(db.DateTime)
+#     billing_address_id: Mapped[str]
+#     shipping_address_id: Mapped[str]
+#
+#     # order_items (association with price snapshot):
+#     # id, order_id, product_id, product_name, sku (optional),
+#     # unit_price, quantity, line_total
+#     #
+#     # (Store product_name/unit_price here to freeze what was bought at that time.)
+#
+#
+# class Addresses(db.Model):
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     user_id: Mapped[int] = mapped_column(nullable=True)
+#     full_name: Mapped[str]
+#     line1: Mapped[str]
+#     line2: Mapped[str]
+#     city: Mapped[str]
+#     region: Mapped[str]
+#     postal_code: Mapped[str]
+#     country: Mapped[str]
+#     phone: Mapped[int] = mapped_column(unique=True)
+#
+#
+# class Payments(db.Model):
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     order_id: Mapped[int] = mapped_column(nullable=True)
+#     provider: Mapped[str]
+#     provider_ref: Mapped[str]
+#     amount: Mapped[int]
+#     currency: Mapped[str]
+#     status: Mapped[str]
+#     captured_at: Mapped[str]
+#
+#
+# class Shipments(db.Model):
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     order_id: Mapped[int] = mapped_column(nullable=True)
+#     carrier: Mapped[str]
+#     service: Mapped[str]
+#     tracking_number: Mapped[str]
+#     status: Mapped[str]
+#     shipped_at: Mapped[str]
+#     delivered_at: Mapped[str]
 
     # (Optional later: reviews, coupons/promotions, wishlists, inventory by location, product_variants for size/color)
 
